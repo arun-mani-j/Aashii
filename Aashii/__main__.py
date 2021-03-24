bd2d53fbbfade469abe8b09b2496e468ae6fd340
J Arun Mani <j_arun_mani@VaioZone>
2021-03-24 23:39:45 +0530

    Fixed Pylint warning : W1508.

--- Aashii/__main__.py
+++ Aashii/__main__.py
@@ -6,9 +6,9 @@ from Aashii.handlers import handlers
 
 token = getenv("TOKEN")
 database_url = getenv("DATABASE_URL")
-interval = int(getenv("POLL_INTERVAL", 1))
+interval = int(getenv("POLL_INTERVAL", "1"))
 listen = getenv("LISTEN")
-port = int(getenv("PORT", 0))
+port = int(getenv("PORT", "0"))
 url = getenv("URL")
 server = Server(token, database_url, handlers)
 
