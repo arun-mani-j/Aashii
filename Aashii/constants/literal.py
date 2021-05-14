450909a2f513f2ba6cccab28908078e45ef1677f
J Arun Mani <j.arunmani@protonmail.com>
2021-05-14 18:16:39 +0530

    Fixed typo

--- Aashii/constants/literal.py
+++ Aashii/constants/literal.py
@@ -8,7 +8,7 @@ import os
 class Literal:
 
     """
-    Lieral contains values that are simple and never change, like admins group ID.
+    Lieral contains values that are simple and never change, like admins' group ID.
     """
 
     ADMINS_GROUP_ID = os.getenv("ADMINS_GROUP_ID")
