1c28448ef342e13dc270b7e3408c35da6484bfca
J Arun Mani <j.arunmani@protonmail.com>
2021-05-21 21:55:28 +0530

    Removed blocking method of forwarding message to admins

--- Aashii/bridge/user.py
+++ Aashii/bridge/user.py
@@ -1,8 +1,16 @@
-from time import sleep
 from telegram import ChatAction, Update
 from telegram.ext import CallbackContext
 from Aashii.constants import Literal
 from Aashii.utils.wrappers import check_user_status
+
+
+def __send_admins(context: CallbackContext):
+
+    database = context.bot_data["database"]
+    message = context.job.context
+    user_id = message.from_user.id
+    message = message.forward(Literal.ADMINS_GROUP_ID)
+    database.add_message(message.message_id, user_id)
 
 
 @check_user_status
@@ -12,11 +20,9 @@ def forward_to_admins(update: Update, context: CallbackContext):
     Sends the user's message to admins.
     """
 
-    database = context.bot_data["database"]
-    user_id = update.message.from_user.id
     context.bot.send_chat_action(
         chat_id=Literal.ADMINS_GROUP_ID, action=ChatAction.TYPING
     )
-    sleep(Literal.DELAY_SECONDS)
-    message = update.message.forward(Literal.ADMINS_GROUP_ID)
-    database.add_message(message.message_id, user_id)
+    context.job_queue.run_once(
+        callback=__send_admins, when=Literal.DELAY_SECONDS, context=update.message
+    )
